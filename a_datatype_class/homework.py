08acb157df8718d060755d3f6ade760bfb98f83a
yoobinbkk <yoobinbkk@gmail.com>
2022-12-05 00:33:34 +0900

    2nd Commit - 2022.12.05 주일

--- a_datatype_class/homework.py
+++ a_datatype_class/homework.py
@@ -1,7 +1,9 @@
 import math
 import numpy as np
 
+
 # 1
+
 # a = int(input('정수를입력하세요: '))
 # b = int(input('정수를입력하세요: '))
 # c = int(input('정수를입력하세요: '))
@@ -10,21 +12,43 @@ import numpy as np
 #
 # print('평균 =', (a+b+c+d+e)/5)
 
+# -------------------------------------
+
+# sum, num = 0, 5
+# for i in range(num):
+#     sum += int(input('정수를입력하세요: '))
+#
+# print('평균 :', sum/num)
+
+# -------------------------------------
+
+# lt = [int(i) for i in input('정수를입력하세요: ').split()]
+# print("평균=", sum(lt)/len(lt))
+
+# -------------------------------------
+
+# lt = []
+# while(len(lt)!=5):
+#     lt.append(int(input('정수를입력하세요: ')))
+#
+# print("평균=", sum(lt)/len(lt))
+
+# ************************************************
+
 
 # 2
-# result = input('문자열을 입력하세요>>')
-# print(result[::-1])
 
+# result = input('문자열을 입력하세요>>')
+# print('결과 :', result[::-1])
+
+# -------------------------------------
+
+# print('결과 :', input('문자열을 입력하세요>>')[::-1])
+
+# *************************************************************
 
 
 # 3
-
-# *************************************************************
-# sum = 0
-# list = [int(i) for i in input('입력>>>').split()]
-# print('평균 : {0}, 표준편차 : {1}'.format(np.mean(list), np.std(list)))
-# *************************************************************
-
 
 # numbers = input('정수리스트입력:')     #입력값 받아오기
 # nums = numbers.split()              #list로 변환
@@ -44,23 +68,15 @@ import numpy as np
 # print('평균= {}'.format(avg))
 # print('표준편차{:.2f}'.format(bb))
 
+# -------------------------------------
+
+# list = [int(i) for i in input('정수리스트입력>>>').split()]
+# print('평균 : {0}, 표준편차 : {1:.2f}'.format(np.mean(list), np.std(list)))
+
+# *************************************************************
+
 
 # 4
-
-l = list(input('문자 입력>>>'))
-
-alphabet = "ABC DEF GHI JKL MNO PQRS TUV WXYZ"
-alp = alphabet.split()
-
-print(l)
-print(alp)
-
-ans = ""
-for x in range(len(l)):
-    for i in range(len(alp)):
-        ans += alp[i].find(l[x])
-
-print(ans)
 
 # phonenum =[[],['A','B','C'],['D','E','F'], ['G','H','I'],['J','K','L'],['M','N','O'],
 #            ['P','Q','R','S'],['T','U','V'], ['W','X','Y','Z']]
@@ -73,3 +89,17 @@ print(ans)
 #             # 숫자값끼리 더하면 합한 값이 나오기 때문에 str로 형 변환
 #             result += str(i+1)
 # print(result)
+
+# ----------------------------------------------------
+
+# ls = list(input('문자열을 입력하시오: '))
+# alphabet = "ABC DEF GHI JKL MNO PQRS TUV WXYZ".split()
+#
+# result = ""
+# for i in range(len(ls)):
+#     for j in range(len(alphabet)):
+#         if ls[i].upper() in alphabet[j]:
+#             result += str(j+2)
+#
+# print(result)
+
---
+++ a_datatype_class/practice.py
@@ -0,0 +1,10 @@
+# for 문으로 구구단을 만들기
+
+for i in range(1,10):
+    for j in range(1,10):
+        print(i*j, end=" ")
+    print()
+
+
+# 리스트 컴프리헨션
+
